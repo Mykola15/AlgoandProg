3f694639b7a3a20f51ba9f5fb33c81a5f7d056d5
Mykola15 <43365006+Mykola15@users.noreply.github.com>
2018-11-14 15:37:40 +0200

    Add files via upload

---
+++ Lab_6/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int wordsnumber = 0, wordcheck;
+    size_t lengthtext;
+    int j = 1;
+    char text[256], delimiter[7] = " .,:;-\n";
+    int counter=0;
+    fgets(text, 256, stdin);
+    if (text[strlen(text) - 1] == '\n')
+    text[strlen(text) - 1] = '\0';
+
+    char *word=strtok(text," ");
+    char *word1=word;
+    while (word!=NULL)
+    {
+        if(strcmp(word1,word)==0)
+        {
+            counter+=1;
+        }
+        word=strtok(NULL," ");
+    }
+    printf("\n%s = %d times",word1,counter);
+}
